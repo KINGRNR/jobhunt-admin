90a26c2ba823e6322b4d2323342420e1362bf45b
Rafif <rafifnr06@gmail.com>
2023-10-05 21:29:49 +0700

    Update datatable style,

--- resources/views/managejob/index.blade.php
+++ resources/views/managejob/index.blade.php
@@ -1,44 +1,72 @@
 <div class="table-user-ini">
-    <div class="row mb-5 w-75 mx-auto">
-        <div class="bg-white rounded-3 p-7 shadow-sm m-3">
-            <div class="d-flex">
-                <input type="date" name="filter_date" id="filter_date" class="form-control form-control-sm"
-                    style="width: 20%!important;" onchange="onFilter()">
-                <select name="status" id="status" class="form-select form-select-sm form-select-solid ms-2"
-                    style="width: 20%!important;">
-                    <option value="">Status</option>
-                    <option value="approve">Approved</option>
-                    <option value="reject">Rejected</option>
-                    <option value="processing">Processing</option>
-                </select>
+
+    <div class="card">
+        <!--begin::Card header-->
+        <div class="card-header py-4">
+            <div class="card-title">
+                {{-- <h1
+                    style="color: var(--txt, #323232);
+                font-size: 20px;
+                font-style: normal;
+                font-weight: 600;
+                line-height: 140%; /* 28px */
+                letter-spacing: 0.2px;">
+                    List User</h1> --}}
                 <div class="input-group ms-2">
                     <span class="input-group-text" id="basic-addon1">
                         <i class="align-self-center fs-2 las la-search"></i>
                     </span>
-                    <input type="search" name="search_example" id="search_example" placeholder="Search Here!"
-                        class="form-control form-control-sm" autocomplete="off">
-
+                    <input type="search" name="search_user" id="search_user" placeholder="Cari"
+                        class="form-control form-control-sm" autocomplete="off" style="display: flex;
+                        height: 48px;
+                        flex-direction: column;
+                        align-items: flex-start;
+                        gap: 8px;
+                        flex: 1 0 0;
+                        width: 241px;">
                 </div>
-
-
             </div>
+            <div class="card-toolbar">
+                    <div class="d-flex">
+                        <input type="date" name="filter_date" id="filter_date" class="form-control form-control-sm me-2"
+                            style="display: flex;
+                            width: 141px;
+                            height: 48px;
+                            padding: 10px 16px;
+                            align-items: center;
+                            gap: 16px;" onchange="onFilter()">
+                        <select name="status" id="status" class="form-select form-select-sm form-select-solid ms-2"
+                            style="display: flex;
+                            width: 141px;
+                            height: 48px;
+                            padding: 10px 16px;
+                            align-items: center;
+                            gap: 16px;">
+                            <option value="">Status</option>
+                            <option value="approve">Approved</option>
+                            <option value="reject">Rejected</option>
+                            <option value="processing">Processing</option>
+                        </select>
+                    </div>
 
+                    <div class="fw-bolder me-3 ms-2 deleted-selected" style="display: none;">
+                        <span class="me-2" id="selected_total">10</span>Selected
+                    </div>
+                    <button type="button" class="btn btn-sm btn-danger deleted-selected" data-kt-customer-table-select="delete_selected" style="display: none;"
+                        onclick="deleteSelected()">Delete Selected</button>
+            </div>
         </div>
-    </div>
-
-    <div class="card">
-        <!--begin::Card header-->
 
         <!--end::Card header-->
         <!--begin::Card body-->
         <div class="card-body pt-0">
 
             <!--begin::Table-->
-            <table class="table align-middle table-row-dashed fs-6 gy-5 text-center" id="table-user">
+            <table class="table align-middle table-hover  table-row-dashed fs-6 gy-5 text-center" id="table-user">
                 <!--begin::Table head-->
                 <thead>
                     <!--begin::Table row-->
-                    <tr class="text-center align-middle text-gray-400 fw-bolder fs-7 text-uppercase gs-0">
+                    <tr class="text-start align-middle text-gray-400 fw-bolder fs-7 text-uppercase gs-0">
                         <th class="ps-4" width="20">No</th>
                         <th class="min-w-20px">Photo</th>
                         <th class="min-w-125px">Joining date</th>
@@ -50,7 +78,7 @@
                     </tr>
                     <!--end::Table row-->
                 </thead>
-                <tbody class="fw-bold text-gray-600 text-center align-middle">
+                <tbody class="fw-bold text-gray-600 text-start align-middle">
                     <tr>
                         <td>1</td>
                         <td><img src="" alt=""></td>
